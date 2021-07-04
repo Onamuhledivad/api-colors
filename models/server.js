221af2291e78c1b6620af00cf45bd438481e6dec
Onamuhledivad <david.romero.ti@gmail.com>
2021-07-04 03:35:05 -0500

    env eliminado

--- models/server.js
+++ models/server.js
@@ -7,7 +7,7 @@ class Server {
     constructor() {
         this.app = express();
         this.port = process.env.PORT;
-        this.apiPath = '/api/v1/';
+        this.apiPath = '/api/v1';
 
         //Conexion a la BD
         this.conectarDB();
